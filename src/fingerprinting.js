b0b1a71a6c4377eee1f084199e54be79b564b8e0
MisterCob <opitmili@gmail.com>
2025-05-25 13:40:21 -0400

    Update fingerprinting.js

--- src/fingerprinting.js
+++ src/fingerprinting.js
@@ -1,5 +1,9 @@
-// Integrate FingerprintJS here
-
-new FingerprintJS().get(function(result) {
-    console.log(result);  // Fingerprint data output
-});
+//Integrate FingerprintJS here
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fingerprintjs/3.3.0/fingerprint.min.js"></script>
+<script>
+    if (localStorage.getItem('cookie-consent') === 'granted' && localStorage.getItem('fingerprint-consent') === 'granted') {
+        new FingerprintJS().get(function(result) {
+            console.log(result);  // The fingerprint data
+        });
+    }
+</script>
